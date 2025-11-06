5f44046c9d81738146f74207d5835afc44b1e992
elango05-coder <elangodevanathan2007@gmail.com>
2025-11-06 18:43:13 +0530

    Add files via upload

---
+++ hope cart/script.js
@@ -0,0 +1,17 @@
+function loginUser(event) {
+  event.preventDefault();
+  alert("Login successful! Redirecting to Home page...");
+  window.location.href = "index.html";
+}
+
+function submitExchange(event) {
+  event.preventDefault();
+  alert("Exchange form submitted successfully!");
+  window.location.href = "index.html";
+}
+
+function submitContact(event) {
+  event.preventDefault();
+  alert("Thanks for contacting HopeCart!");
+  window.location.href = "index.html";
+}
